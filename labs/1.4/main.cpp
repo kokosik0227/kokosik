db41ac6fa86a7370d294b43ffda1e3758a5d9ad2
kokosik <niks130207@gmail.com>
2024-09-30 11:06:21 +0300

    bro

--- labs/1.4/main.cpp
+++ labs/1.4/main.cpp
@@ -1,96 +1,54 @@
-/**
- * Done by:
- * Student Name: Severyn Kotyhoroshko
- * Student Group: 123
- * Lab 1.4
- */
-
 #include <iostream>
 
-using namespace std;
 
-int main() {
-    // Comment to local scope 1
-    {
-        int num = 10;
-        int* ptr = &num; // ptr now holds the address of num
+int main()
+{
+	unsigned short nA;
+	int nB;
+	float nC;
+	double nD;
+	// Опис змінних напередвизначених типів
 
-        cout << "Value of num: " << num << endl;        // Outputs: 10
-        cout << "Address of num: " << ptr << endl;      // Outputs the memory address of num
-        cout << "Value at the address stored in ptr: " << *ptr << endl; // Outputs: 10
+	unsigned short* pnA;
+	int* pnB;
+	float* pnC;
+	double* pnD;
+	//Опис вказівних змінних відповідних типів
 
-        num++;
-        cout << num << endl;
+	void* p_void;
+	//Опис нетипізованої вказівної змінної
 
-        // delete ptr;         // Deallocates the memory
-        ptr = nullptr;         // Good practice to avoid dangling ptrs
-        cout << "Nullified ptr: " << ptr << endl;
-    }
+	pnA = &nA;
+	pnB = &nB;
+	pnC = &nC;
+	pnD = &nD;
+	//Ініціювання вказівних змінних адресами змінних
 
-    // Comment to local scope 2
-    {
-        cout << "Pointer Arithmetic" << endl;
-        int arr[] = {1, 2, 3, 4, 5};
+	*pnA = 45671;
+	*pnB = 167;
+	*pnC = -3.9412;
+	*pnD = -6.8e-6;
+	// Ініціювання змінних значеннями, використовуючи операцію розіменування вказівних змінних
 
-        cout << "sizeof arr = " << sizeof(arr) << endl;
-        int* ptr = arr;         // Points to the first element of the array
+	int sizeShort = sizeof(nA);
+	int sizeShortP = sizeof(pnA);
+	int sizeInt = sizeof(nB);
+	int sizeIntP = sizeof(pnB);
+	int sizeFloat = sizeof(nC);
+	int sizeFloatP = sizeof(pnC);
+	int sizeDouble = sizeof(nD);
+	int sizeDoubleP = sizeof(pnD);
+	int sizeVoid = sizeof(p_void);
+	//Визначення розміру всіх змінних програми
 
-        const int firstEl = arr[0];
+	p_void = pnA;
+	p_void = pnB;
+	p_void = pnC;
+	p_void = pnD;
+	//Ініціювання нетипізованої вказівної змінної адресами типізованих вказівних змінних
 
-        cout << arr[0] << endl;   // Outputs: 1
-        cout << ptr << endl;   // Outputs: Address
-        cout << *ptr << endl;   // Outputs: 1
-        ptr++;                  // Moves to the next element
-        cout << *ptr << endl;   // Outputs: 2
+	int& linkB = nB;
+	//Опис посилання
 
-        ptr = nullptr;         // Good practice to avoid dangling ptrs
-        cout << "sizeof arr = " << sizeof(arr) << endl;
-    }
-
-    // Comment to local scope 3
-    {
-        cout << "Dynamic Memory Allocation for INT" << endl;
-        int* ptr = new int;    // Allocates memory for a single integer
-        *ptr = 20;             // Sets the value of the allocated memory
-
-        cout << *ptr << endl;  // Outputs: 20
-
-        delete ptr;            // Deallocates the memory
-        ptr = nullptr;         // Good practice to avoid dangling ptrs
-    }
-
-
-    // Comment to local scope 4
-    {
-        cout << "Dynamic Memory Allocation for ARR" << endl;
-        int* arr = new int[5]; // Allocates memory for an array of 5 integers
-
-        cout << "sizeof *arr = " << sizeof(*arr) << endl;
-        cout << "sizeof arr = " << sizeof(arr) << endl;
-
-        cout << arr << endl;
-        for(int i = 0; i < 5; i++) {
-            arr[i] = i + 1;    // Initializes array
-        }
-
-        delete[] arr;          // Deallocates the array memory
-        arr = nullptr;
-        cout << arr << endl;
-        cout << "sizeof *arr = " << sizeof(*arr) << endl;
-        cout << "sizeof arr = " << sizeof(arr) << endl;
-    }
-
-    // Comment to local scope 5
-    {
-        cout << "Pointers to Pointers" << endl;
-        int num = 10;
-        int* ptr = &num;
-        int** ptrToPtr = &ptr;
-
-        cout << **ptrToPtr << endl; // Outputs: 10
-        ptr = nullptr;
-        ptrToPtr = nullptr;
-    }
-
-    return 0;
+	return 0;
 }
