ec46646bf531d51e43cb65dc63dc7e0f3f4210cf
kokosik <niks130207@gmail.com>
2024-10-24 23:29:05 +0300

    bro

--- labs/1.6/main.cpp
+++ labs/1.6/main.cpp
@@ -1,37 +1,15 @@
 #include <iostream>
 
-int main() {
-    setlocale(LC_ALL, "uk-UA");
-    // 1. Опис змінних символьного типу
-    char charVar1;
-    char charVar2;
-    char charVar3;
+int main()
+{
+	char chVar1;
+	chVar1 = 'R';
+	char chVar2 = '7';
 
-    // 2. Опис констант символьного типу
-    const char CHAR_CONST1 = '>';
-    const char CHAR_CONST2 = '7';
-    const char CHAR_CONST3 = 'R';
-    const char CHAR_CONST4 = 0x1a; // Control character (Substitute) - might not display
-    const char CHAR_CONST5 = 0x32; // '2'
-    const char CHAR_CONST6 = 0x63; // 'c'
+	char chVar3;
+	chVar3 = 0x32; // символ '2' в таблиці ASCII
+	char chVar4 = 0x63; // символ 'c' в таблиці ASCII
 
-
-    // 3. Ініціалізація об'єктів при описі
-    char charVar4 = 'A';
-    char charVar5 = 0x42; // 'B'
-
-    // 4. Ініціалізація об'єктів оператором присвоювання
-    charVar1 = CHAR_CONST1;
-    charVar2 = CHAR_CONST2;
-    charVar3 = CHAR_CONST3;
-
-    char charVar6;
-    char charVar7;
-    char charVar8;
-
-    charVar6 = CHAR_CONST4;
-    charVar7 = CHAR_CONST5;
-    charVar8 = CHAR_CONST6;
-
-    return 0;
+	const char CONST1 = '>';
+	const char CONST2 = 0x1a; // SUB в таблиці ASCII
 }
