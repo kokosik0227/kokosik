56912e8de8393cb04bad13e59ae1c9b60e96ec09
kokosik <niks130207@gmail.com>
2024-10-25 06:04:58 +0300

    bro

--- labs/1.6/main.cpp
+++ labs/1.6/main.cpp
@@ -12,4 +12,51 @@ int main()
 
 	const char CONST1 = '>';
 	const char CONST2 = 0x1a; // SUB в таблиці ASCII
+
+	// Part 2
+
+// Task 1: Опис змінних int, float, unsigned short
+	int a;
+	float b;
+	unsigned short c;
+
+	// Task 2: Ініціалізація змінних з п.1 
+	a = 682;
+	b = -8.4e2; // 1.23 * 10^4 = 12300.0
+	c = 25921;
+
+	// Task 3: Опис змінних double, int, char
+	double d;
+	int e;
+	char f;
+
+	// Task 4.1: Ініціалізація з неявним приведенням типів
+	d = a;    // int -> double
+	e = b;    // float -> int (втрата дробової частини)
+	f = c;    // unsigned short -> char (можлива втрата даних, якщо c > 127)
+
+
+	// Task 4.2: Ініціалізація з явним приведенням типів
+	d = (double)a;
+	e = (int)b;
+	f = (char)c;
+
+
+	// Task 4.3: Ініціалізація з обходом суворої типізації 
+	void* pV;  // void покажчик (нетипізований)
+
+	double* pD;
+	pV = &a;       // pV вказує на адресу змінної a
+	pD = (double*)pV; // pD (типізований) вказує туди ж, куди pV
+	d = *pD;     // копіювання значення за адресою, на яку вказує pD, в d
+
+	int* pE;
+	pV = &b;
+	pE = (int*)pV;
+	e = *pE;
+
+	char* pF;
+	pV = &c;
+	pF = (char*)pV;
+	f = *pF;
 }
